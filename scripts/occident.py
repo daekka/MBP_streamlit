f29d0794e95f40b85798640475e1cf97d358250b
daekka <daekka@gmail.com>
2025-04-06 18:09:07 +0200

    Mejorar el procesamiento de recibos en la función `procesar_OCCIDENT` al agregar la conversión a mayúsculas y el recorte de espacios en los campos relevantes. Se optimizó la lógica de filtrado para los recibos de la compañía.

--- scripts/occident.py
+++ scripts/occident.py
@@ -18,12 +18,11 @@ def procesar_OCCIDENT():
     st.session_state.df_OCCIDENT['polizas'] = procesar_polizas(compañia, st.session_state.df_origen_compañias['df_occident_polizas'], st.session_state.df_origen_compañias['df_occident_clientes'], st.session_state.df_OCCIDENT['polizas'], columna_cliente_poliza_origen, columna_cliente_clientes_origen, columna_id_polizas)
 
     # Procesar recibos
-    
-    st.session_state.df_origen_compañias['df_occident_recibos'] = st.session_state.df_origen_compañias['df_occident_recibos']  [
-        (st.session_state.df_origen_compañias['df_occident_recibos']['Agente'].str.strip() == 'M832793V') & 
-        (st.session_state.df_origen_compañias['df_occident_recibos']['Situación del recibo'].isin(['Cobrado', 'Pendiente']))
+    st.session_state.df_origen_compañias['df_occident_recibos'] = st.session_state.df_origen_compañias['df_occident_recibos'][
+        (st.session_state.df_origen_compañias['df_occident_recibos']['Agente'].str.strip().str.upper() == 'M832793V') & 
+        (st.session_state.df_origen_compañias['df_occident_recibos']['Situación del recibo'].str.strip().isin(['Cobrado', 'Pendiente']))
     ]
-    st.dataframe(st.session_state.df_origen_compañias['df_occident_recibos'])
+    
     fecha_orden = "Fecha emisión";
     prima_orden = "Prima total";
     st.session_state.df_origen_compañias['df_occident_recibos'] = st.session_state.df_origen_compañias['df_occident_recibos'].sort_values(by=[fecha_orden, prima_orden], ascending=[False, False])
